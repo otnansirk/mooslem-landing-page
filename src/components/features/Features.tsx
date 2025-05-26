cc7b05725a5baccd53fba51e67eab2184e7a4f70
otnansirk <iam.otnansirk@gmail.com>
2025-05-26 20:26:49 +0700

    fix : features sections

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -1,4 +1,53 @@
+import { CheckIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
+
+
+const features = [
+    {
+        name: 'Adhan Notifications',
+        description: 'Get notified with Adhan sound before the next prayer time.'
+    },
+    {
+        name: 'Customizable Clock',
+        description: 'A clean and customizable clock that helps you keep track of time while staying productive.'
+    },
+    {
+        name: 'Weather',
+        description: 'Real-time weather updates tailored to your current location.'
+    },
+    {
+        name: 'Greetings',
+        description: 'Personalized greetings that uplift your mood and set the tone for your day.'
+    },
+    {
+        name: 'Notes',
+        description: 'A simple space to jot down reminders, tasks, or reflections directly on your start page.'
+    },
+    {
+        name: 'Customizable Notifications',
+        description: 'Enable or disable Adhan and prayer alerts based on your preference.'
+    },
+    {
+        name: 'Dark Mode (Coming Soon)',
+        description: 'A sleek, eye-friendly dark theme to reduce strain during nighttime use.'
+    },
+    {
+        name: 'Al-Quran Integration (Coming Soon)',
+        description: 'Read and reflect on the Quran directly from your new tab page.'
+    },
+    {
+        name: 'Inspirational Islamic Quotes (Coming Soon)',
+        description: 'Daily Islamic quotes to inspire your soul and keep you grounded.'
+    },
+    {
+        name: 'Multilingual Support (Coming Soon)',
+        description: 'Experience Mooslem Start Page in your preferred language.'
+    },
+    {
+        name: 'Quick Access Links (Coming Soon)',
+        description: 'Access your favorite websites and Islamic resources with one click.'
+    }
+]
 
 
 export default function Features() {
@@ -39,6 +88,32 @@ export default function Features() {
                         <div className="lg:ml-auto lg:pt-4 lg:pl-4 flex items-center">
                             <div className="lg:max-w-lg">
                                 <h2 className="mt-2 text-3xl tracking-tight text-pretty sm:text-4xl">
+                                    Dynamic Background
+                                </h2>
+                                <p className="mt-4 text-lg/8 text-gray-300">
+                                    Mooslem Start Page brings a new aesthetic with every click—featuring dynamic 4K backgrounds that elevate your workspace, help you stay focused, and add a calming visual experience to your daily flow.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex items-start justify-end order-first">
+                            <Image
+                                alt="Product screenshot"
+                                src="/images/background.png"
+                                width={832}
+                                height={442}
+                                className="w-3xl max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-228 md:-ml-4 lg:-ml-0"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="overflow-hidden py-16 sm:py-30">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-4 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                        <div className="lg:pt-4 lg:pr-8 flex items-center order-2 sm:order-1">
+                            <div className="lg:max-w-lg">
+                                <h2 className="mt-2 text-3xl tracking-tight text-pretty sm:text-4xl">
                                     Customizable
                                 </h2>
                                 <p className="mt-4 text-lg/8 text-gray-300">
@@ -46,15 +121,40 @@ export default function Features() {
                                 </p>
                             </div>
                         </div>
-                        <div className="flex items-start justify-end order-first">
-                            <Image
-                                alt="Product screenshot"
-                                src="/images/dynamic-bg.png"
-                                width={832}
-                                height={442}
-                                className="w-3xl max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-228 md:-ml-4 lg:-ml-0"
-                            />
+                        <Image
+                            alt="Product screenshot"
+                            src="/images/customizable.png"
+                            width={832}
+                            height={442}
+                            className="w-3xl max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-228 md:-ml-4 lg:-ml-0 order-1 sm:order-2"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className="py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+                        <div className="col-span-2">
+                            <h2 className="text-base/7 font-semibold text-gray-300">Everything you need</h2>
+                            <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-200 sm:text-5xl">
+                                More Features, More Focus
+                            </p>
+                            <p className="mt-6 text-base/7 text-gray-300">
+                                Mooslem Start Page is designed to enhance your daily routine with features that keep you organized, focused, and connected to your faith. From prayer times to dynamic backgrounds, every element is crafted to support your productivity and spiritual journey.
+                            </p>
                         </div>
+                        <dl className="col-span-3 grid grid-cols-1 gap-x-8 gap-y-10 text-base/7 text-gray-200 sm:grid-cols-2 lg:gap-y-16">
+                            {features.map((feature) => (
+                                <div key={feature.name} className="relative pl-9">
+                                    <dt className="font-semibold text-gray-100">
+                                        <CheckIcon aria-hidden="true" className="absolute top-1 left-0 size-5 text-gray-100" />
+                                        {feature.name}
+                                    </dt>
+                                    <dd className="mt-2">{feature.description}</dd>
+                                </div>
+                            ))}
+                        </dl>
                     </div>
                 </div>
             </div>
