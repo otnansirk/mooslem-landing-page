56f44d56bf59164cceb3532f30b9cce51397ae8e
Kris <iam.otnansirk@gmail.com>
2025-05-27 14:10:25 +0700

    fix : button

--- src/components/form/Button.tsx
+++ src/components/form/Button.tsx
@@ -6,7 +6,7 @@ type ButtonType = {
 }
 
 const Button = (props: ButtonType) => {
-  return <button onClick={props.onClick} className={`flex items-center justify-center gap-[0.7em] max-w-[260px] px-5 py-3 rounded-[12px] border border-white/10 bg-white/10 text-inherit whitespace-nowrap box-border backdrop-blur-md hover:bg-white/20 ${props.className || ''}`}>
+  return <button onClick={props.onClick} className={`flex items-center justify-center gap-[0.7em] max-w-[260px] px-5 py-3 rounded-[12px] border border-white/10 whitespace-nowrap box-border backdrop-blur-md ${props.className || 'bg-white/10 text-inherit hover:bg-white/20'}`}>
     {props.children}
   </button>
 }
