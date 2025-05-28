b8640e8297fa8c3d474e1f6c73d479c33e7d5ee0
Kris <iam.otnansirk@gmail.com>
2025-05-28 10:40:53 +0700

    fix : remove await

--- src/app/[locale]/docs/[slug]/page.tsx
+++ src/app/[locale]/docs/[slug]/page.tsx
@@ -3,7 +3,7 @@ import NotionRender from "@/components/docs/NotionRender"
 import { getDocsNavigationByLocale } from "@/utils/docs-navigation";
 
 const DocsDetail = async ({ params }: { params: { slug: string, locale: string } }) => {
-    const { slug, locale } = await params;
+    const { slug, locale } = params;
     const pageId = getDocsNavigationByLocale(locale ?? "en").find(item => item.href === `/docs/${slug}`)?.pageId ?? "";
 
     return (
